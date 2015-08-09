896fea784ac1a93a599e6eba71e0ecad6e2d2511
ewnd9 <ewndnine@gmail.com>
2015-08-09 14:09:57 +0300

    add google services links in menu

--- cli.js
+++ cli.js
@@ -6,9 +6,18 @@ inquirer.prompt({
   type: 'list',
   name: 'service',
   message: 'Select service',
-  choices: ['github']
+  choices: [
+    'github',
+    new inquirer.Separator(),
+    'google-analytics',
+    'google-webmaster'
+  ]
 }).then(function(result) {
   if (result.service === 'github') {
     require('./lib/github/main')();
+  } else if (result.service === 'google-analytics') {
+    require('./lib/utils/open-url')('google-analytics');
+  } else if (result.service === 'google-webmaster') {
+    require('./lib/utils/open-url')('google-webmaster');
   }
 });
