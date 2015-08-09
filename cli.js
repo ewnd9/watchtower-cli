410365502bd3eba6c021df2ae85a49397a87120c
ewnd9 <ewndnine@gmail.com>
2015-08-09 15:13:39 +0300

    add custom binary to menu feature

--- cli.js
+++ cli.js
@@ -3,35 +3,9 @@
 var inquirer = require('inquirer-bluebird');
 var _ = require('lodash');
 
-var config = require('./lib/utils/config');
-
-var github = {
-  name: 'github',
-  value: function() {
-    return require('./lib/github/main')();
-  }
-};
-
-var links = _.map(config.data.links, function(url, name) {
-  return {
-    name: name,
-    value: function() {
-      console.log('opening ' + url);
-      require('opn')(url);
-    }
-  };
-});
-
-links.push({
-  name: 'Add link',
-  value: function() {
-    return require('./lib/utils/links-prompt')();
-  }
-});
-
-var choices = [github, new inquirer.Separator()].concat(links);
-
 var argv = require('minimist')(process.argv.slice(2));
+
+var choices = require('./lib/utils/generate-menu');
 
 var service = _.find(choices, function(choice) {
   return _.find(argv, function(value, flag) {
